a604d2d8e2db7b9e92f348b953573bef2f12f777
Kazishayem <kazishayem1234@gmail.com>
2023-02-19 23:46:10 +0600

    navbar

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
 <script setup>
 
+import Navbar from './components/Navbar.vue';
+
+
+
 </script>
 
 <template>
   <div class="container overflow-x-hidden">
 
- Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae quaerat fugiat est eius, facere minus? Aliquid impedit quia nemo voluptas nulla rerum quas ipsum consectetur laborum, esse necessitatibus excepturi ea vero, ipsam repellat? Nobis sunt ipsa nesciunt magni, mollitia cumque voluptates dicta? Totam porro et, voluptatem atque doloremque deleniti impedit.
+<Navbar/>
 
   </div>
 </template>
