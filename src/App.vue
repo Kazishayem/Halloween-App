cd3756ad82508425fe882af3a3a088ab954797fa
Kazishayem <kazishayem1234@gmail.com>
2023-02-19 15:14:10 +0600

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="container overflow-x-hidden">
+
+ Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae quaerat fugiat est eius, facere minus? Aliquid impedit quia nemo voluptas nulla rerum quas ipsum consectetur laborum, esse necessitatibus excepturi ea vero, ipsam repellat? Nobis sunt ipsa nesciunt magni, mollitia cumque voluptates dicta? Totam porro et, voluptatem atque doloremque deleniti impedit.
+
+  </div>
+</template>
+
+<style scoped></style>
